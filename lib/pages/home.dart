2277915a66148a134c6ac1da6e19d6e8894ce1cd
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-06-03 22:06:52 +0530

    Removed boilerplate code.

---
+++ lib/pages/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Be Removed"),
+      ),
+      body: Center(
+        child: Text("Hello There!"),
+      ),
+    );
+  }
+}
